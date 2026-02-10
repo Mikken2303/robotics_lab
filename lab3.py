78a9de7377aa53366b3764cbb1f9ce856535bbdd
Mikken2303 <pallayovci@gmail.com>
2026-02-10 16:07:39 +0000

    lab3 bootstrap

---
+++ lab3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor.lego import GyroSensor, TouchSensor
+from ev3dev2.sensor import INPUT_4, INPUT_1
+
+from time import sleep
