59a734654f08dbb975f5423a5d07442e6fc823e5
Mikken2303 <pallayovci@gmail.com>
2026-02-10 16:06:45 +0000

    lab2 p3

--- lab2.py
+++ lab2.py
@@ -4,20 +4,30 @@ from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor.lego import GyroSensor, TouchSensor
 from ev3dev2.sensor import INPUT_4, INPUT_1
 
+from time import sleep
+
 
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
 gyro = GyroSensor(INPUT_4)
 touch = TouchSensor(INPUT_1)
 
-# drive in a turn for 5 rot
-
-
+tank_drive.on(SpeedPercent(25), SpeedPercent(10))
 
 while(True):
-    tank_drive.on_for_rotations(SpeedPercent(25), SpeedPercent(25), 1)
-    while(gyro.angle < 90):
-        tank_drive.on_for_rotations(SpeedPercent(0), SpeedPercent(25), 0.1)
-    gyro.angle = 0    
+    if(touch.is_pressed):
+        tank_drive.off()
+
+        tank_drive.on_for_seconds(SpeedPercent(-25), SpeedPercent(-25), 1)
+
+        tank_drive.on(SpeedPercent(25), SpeedPercent(-25))
+        g = gyro.angle
+        while(gyro.angle - g < 180):
+            sleep(0.01)
+
+        tank_drive.off()
+        tank_drive.on(SpeedPercent(25), SpeedPercent(10))
+
+    sleep(0.01)    
 
 
 
